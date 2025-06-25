afa5a56b14ea7b87f27c637bec5c2e3330d3fad3
TAB_mk <tabmk.contact@gmail.com>
2025-06-25 22:47:16 +0300

    static fixer

--- svelte.config.js
+++ svelte.config.js
@@ -6,6 +6,11 @@ const config = {
   preprocess: vitePreprocess(),
 
   kit: {
+    paths: {
+      assets: process.env.MODE === 'dev' ? '' : "http://hacked_asset_path",
+      base: "/hacked_base_path",
+      relative: true,
+    },
     adapter: adapter({
       fallback: 'index.html'
     })
